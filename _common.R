c04abc3a61b587e5f9c4e5ed7f7a9934a10e1916
Mauro Loprete <55565968+mauroloprete@users.noreply.github.com>
2024-10-24 01:51:32 -0300

    Closes #29 (#30)
    * Update chapters 2 y 3
    
    * Chapter1: Style code chunk
    
    * Fix cross reference
    
    * Styler
    
    * Fix captions latex
    
    * Update tabla
    
    * Chapters update and lintr
    
    * Lintr
    
    * Renv
    
    * precommit
    
    * Status chapter 3
    
    * Avances
    
    * Avances
    
    * Fix lintr
    
    * Fix gitub develop branch
    
    * Renv again
    
    * Fix deploy
    
    * Chapters/
    
    * Fix develop version metasurvey
    
    * Reference to branch develop
    
    * Fix development version of metasurvey
    
    * Fix renv
    
    * Fix dependence github actions
    
    * Fixes #29
    Renvisolate
    
    * Fixes #29
    
    * By renv y precommit. Fixes #29
    
    * Revert "Fixes #29"
    
    This reverts commit 491cae4821309cda209d0dc960d4a0c64c30e9d2.
    
    * bye recipe
    
    * Renv y precommit
    
    * Fix renv activate

--- _common.R
+++ _common.R
@@ -1,66 +1,64 @@
 status <- function(type) {
-    status = switch(
-        type,
-        borrador = "Este capítulo está en borrador",
-        validacion = "Este capítulo está en proceso de validación",
-        completo = "Este capítulo está completo",
-        borrardor_seccion = "Esta sección está en borrador",
-        proceso = "Este capítulo está en proceso de escritura"
-    )
+  status <- switch(type,
+    borrador = "Este capítulo está en borrador",
+    validacion = "Este capítulo está en proceso de validación",
+    completo = "Este capítulo está completo",
+    borrardor_seccion = "Esta sección está en borrador",
+    proceso = "Este capítulo está en proceso de escritura"
+  )
 
-    class <- switch(type,
-        validacion = "note",
-        borrador = "important",
-        borrador_seccion = "important",
-        completo = "note",
-        proceso = "warning"
-    )
+  class <- switch(type,
+    validacion = "note",
+    borrador = "important",
+    borrador_seccion = "important",
+    completo = "note",
+    proceso = "warning"
+  )
 
-    mensaje <- switch(
-        type,
-        validacion = "Cualquier comentario es bienvenido",
-        borrador = "Revise la rama de desarrollo",
-        borrador_seccion = "Revise la rama de desarrollo",
-        completo = "En caso de encontrar errores, por favor reportarlos",
-        proceso = "Consulte la rama de desarrollo para ver el avance del capítulo"
-    )
+  mensaje <- switch(type,
+    validacion = "Cualquier comentario es bienvenido",
+    borrador = "Revise la rama de desarrollo",
+    borrador_seccion = "Revise la rama de desarrollo",
+    completo = "En caso de encontrar errores, por favor reportarlos",
+    proceso = "Consulte la rama de desarrollo para ver el avance del capítulo"
+  )
 
 
-    cat(paste0(
-        "\n",
-        ":::: status\n",
-        "::: callout-", class, " \n",
-        status, ". ",
-        "\n",
-        mensaje,
-        "\n",
-        ":::\n",
-        "::::\n"
-    ))
+  cat(paste0(
+    "\n",
+    ":::: status\n",
+    "::: callout-", class, " \n",
+    status, ". ",
+    "\n",
+    mensaje,
+    "\n",
+    ":::\n",
+    "::::\n"
+  ))
 }
 
 set.seed(1014)
 
 knitr::opts_chunk$set(
-    comment = "#>",
-    collapse = TRUE,
-    # cache = TRUE,
-    fig.retina = 2,
-    fig.width = 6,
-    fig.asp = 2 / 3,
-    fig.show = "hold"
+  comment = "#>",
+  collapse = TRUE,
+  # cache = TRUE,
+  fig.retina = 2,
+  fig.width = 6,
+  fig.asp = 2 / 3,
+  fig.show = "hold"
 )
 
 options(
-    dplyr.print_min = 6,
-    dplyr.print_max = 6,
-    pillar.max_footer_lines = 2,
-    pillar.min_chars = 15,
-    stringr.view_n = 6,
-    cli.num_colors = 0,
-    cli.hyperlink = FALSE,
-    pillar.bold = TRUE,
-    width = 77 # 80 - 3 for #> comment
+  dplyr.print_min = 6,
+  dplyr.print_max = 6,
+  pillar.max_footer_lines = 2,
+  pillar.min_chars = 15,
+  stringr.view_n = 6,
+  cli.num_colors = 0,
+  cli.hyperlink = FALSE,
+  pillar.bold = TRUE,
+  width = 77 # 80 - 3 for #> comment
 )
 
 ggplot2::theme_set(ggplot2::theme_gray(12))
